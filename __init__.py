78703fc413c3173e18b6b83fdf6fe598529011aa
NadineSiammour25 <nsiammour1@myges.fr>
2025-04-17 11:21:38 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -80,10 +80,6 @@ if __name__ == '__main__':
 
 
 
-                                                                                                                                  
-
-if __name__ == "__main__":
-  app.run(debug=True)
-
+                                                                                                                                
 
 
