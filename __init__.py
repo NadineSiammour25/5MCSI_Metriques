8694a613d328d045ed79458a363f9de72aee50f1
NadineSiammour25 <nsiammour1@myges.fr>
2025-04-17 10:45:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,10 +6,13 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)
-@app.route("/contact/")
+
+@app.route("/contact/") #Formulaire de contact
 def MaPremiereAPI():
     return render_template("contact.html")
-@app.route('/tawarano/')
+
+
+@app.route('/tawarano/') #DonnéeMeteo
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
@@ -20,18 +23,25 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+  
 
-@app.route("/rapport/")
+@app.route("/rapport/")#Rapport
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/histogramme/")#histo
+
+
+@app.route("/histogramme/")#histogramme
 def monhisto():
   return render_template("histogramme.html")
+
+
                                                                                                                                        
-@app.route('/')
+@app.route('/')#helloPage
 def hello_world():
     return render_template('hello.html') #Commit4
+
+
   
 if __name__ == "__main__":
   app.run(debug=True)
