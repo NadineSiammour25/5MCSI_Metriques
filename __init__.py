95bcff3eb75a314ae89866261b91f0773942c686
NadineSiammour25 <nsiammour1@myges.fr>
2025-04-17 11:58:41 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -42,16 +42,15 @@ def monhisto():
   return render_template("histogramme.html")
 
 # Définir le décalage horaire pour Paris (UTC+2 pendant l'heure d'été)
-# Vous pouvez ajuster le décalage si nécessaire, par exemple en UTC+1 en hiver
 UTC_OFFSET = timedelta(hours=2)  # Décalage pour l'heure d'été (Paris)
 
 @app.route('/commits/')
 def commits():
     # Récupérer les commits depuis l'API GitHub avec urllib
     url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
-    response = urlopen(url)  # Utilisation de urllib pour ouvrir l'URL
-    commits_data = json.loads(response.read())  # Lire et décoder la réponse JSON
-    
+    response = urlopen(url)
+    commits_data = json.loads(response.read())
+
     # Créer un dictionnaire pour compter les commits par minute
     commits_per_minute = {}
 
@@ -59,13 +58,13 @@ def commits():
     for commit in commits_data:
         commit_date = commit['commit']['author']['date']  # La date du commit
         commit_time_utc = datetime.strptime(commit_date, "%Y-%m-%dT%H:%M:%SZ")  # Date en UTC
-        
+
         # Convertir UTC en temps local en ajoutant le décalage horaire
         commit_time_local = commit_time_utc + UTC_OFFSET  # Appliquer le décalage horaire
-        
+
         # Extraire la minute du commit
         commit_minute = commit_time_local.strftime('%Y-%m-%d %H:%M')  # Format "2025-04-17 14:35"
-        
+
         # Comptabiliser le commit par minute
         if commit_minute not in commits_per_minute:
             commits_per_minute[commit_minute] = 1
@@ -76,8 +75,9 @@ def commits():
     data = []
     for minute, count in commits_per_minute.items():
         data.append([minute, count])
-    
-    return jsonify(data)
+
+    # Passer les données au template HTML pour générer le graphique
+    return render_template('commits_graph.html', data=data)
 
 
 if __name__ == '__main__':
